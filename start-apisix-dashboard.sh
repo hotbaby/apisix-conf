a11c63b4f7199a61e78307da3f56f883126cbe65
hotbaby <mengyy_linux@163.com>
2021-11-10 09:03:24 +0800

    Update apisix dashboard script

--- start-apisix-dashboard.sh
+++ start-apisix-dashboard.sh
@@ -3,5 +3,6 @@ docker rm apisix-dashboard
 docker run -d --restart always \
               -p 9000:9000 \
               -v `pwd`/dashboard/conf/conf.yaml:/usr/local/apisix-dashboard/conf/conf.yaml \
+              --link etcd:etcd \
               --name apisix-dashboard \
               apache/apisix-dashboard:2.7
