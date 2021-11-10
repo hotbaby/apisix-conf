f8366d4eb30b81f4a948f8766fe493936c532961
hotbaby <mengyy_linux@163.com>
2021-11-10 10:12:29 +0800

    Update apisix script

--- start-apisix.sh
+++ start-apisix.sh
@@ -6,6 +6,13 @@ docker run -d --restart always \
               -v /var/log/apisix:/usr/local/apisix/logs  \
               --link etcd:etcd \
               --link skywalking:skywalking \
+              --link skywalking-ui:skywalking-ui \
+              --link apisix-dashboard:apisix-dashboard \
+              --link httpbin:httpbin \
+              --link registry:registry \
+              --link redis:redis \
+              --link mysql:mysql \
+              --link elasticsearch:elasticsearch \
               -p 9080:9080 \
               -p 9091:9091  \
               -p 9443:9443 \
